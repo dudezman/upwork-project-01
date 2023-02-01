1e0ee4b85fd8cd47ae6c84364e276c50bdd3390d
Amara Montassar <66859425+dudezman@users.noreply.github.com>
2023-02-01 08:23:48 +0100

    done

--- src/app/page/submission/submission.component.ts
+++ src/app/page/submission/submission.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Map, NavigationControl } from 'maplibre-gl';
 @Component({
   selector: 'app-submission',
   templateUrl: './submission.component.html',
   styleUrls: ['./submission.component.scss']
 })
-export class SubmissionComponent implements OnInit {
+export class SubmissionComponent implements OnInit,AfterViewInit {
   data = [
     {
       task:'Work Flow: Requires Location',
@@ -19,7 +19,7 @@ export class SubmissionComponent implements OnInit {
       task:'Work Flow: Requires Location',
       from:'denisgordiyenya@gmail.com',
       to:'denisgordiyenya@gmail.com',
-      status:'Uncomplete',
+      status:'Low Risk',
       address:'2118 Thornridge Cir. Syracuse, Connecticut 35624',
       dueDate: new Date('Oct 6 2023, 02:38 AM')
     },
@@ -35,14 +35,32 @@ export class SubmissionComponent implements OnInit {
       task:'Work Flow: Requires Location',
       from:'denisgordiyenya@gmail.com',
       to:'denisgordiyenya@gmail.com',
-      status:'Uncomplete',
+      status:'Needs Review',
       address:'2118 Thornridge Cir. Syracuse, Connecticut 35624',
       dueDate: new Date('Oct 6 2023, 02:38 AM')
     }
   ]
+
+  @ViewChild('map')
+  private mapContainer!: ElementRef<HTMLElement>;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  ngAfterViewInit() {
+    const myAPIKey = 'fcccdf8a978c42729e19d79a1e414e2e'; 
+    const mapStyle = 'https://maps.geoapify.com/v1/styles/positron/style.json';
+
+    const initialState = { lng: 11, lat: 49, zoom: 9 };
+
+    const map = new Map({
+      container: this.mapContainer.nativeElement,
+      style: `${mapStyle}?apiKey=${myAPIKey}`,
+      center: [initialState.lng, initialState.lat],
+      zoom: initialState.zoom
+    });
+
+    map.addControl(new NavigationControl());
   }
 
 }
--- src/app/page/submission/submission.module.ts
+++ src/app/page/submission/submission.module.ts
@@ -3,15 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { SubmissionRoutingModule } from './submission-routing.module';
 import { SubmissionComponent } from './submission.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule} from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatIconModule} from '@angular/material/icon';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule} from '@angular/material/select';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatButtonModule} from '@angular/material/button';
-import {GMapModule} from 'primeng/gmap';
+import { MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
     SubmissionComponent
@@ -27,8 +26,7 @@ import {GMapModule} from 'primeng/gmap';
     MatSelectModule,
     MatDatepickerModule,
     MatButtonToggleModule,
-    MatButtonModule,
-    GMapModule
+    MatButtonModule
   ]
 })
 export class SubmissionModule { }
